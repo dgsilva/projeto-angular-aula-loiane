1ad33a7ed7beb41042566af4c22aa7b137f10e91
dgsilva <diegorj93@gmail.com>
2022-11-02 20:20:52 -0300

    Criação do componenet Courses

---
+++ src/app/courses/courses-routing.module.ts
@@ -0,0 +1,8 @@
+import { Routes, RouterModule } from '@angular/router';
+import { CoursesComponent } from './courses/courses.component';
+
+const routes: Routes = [
+  {path: '', component: CoursesComponent  },
+];
+
+export const CoursesRoutingRoutes = RouterModule.forChild(routes);
